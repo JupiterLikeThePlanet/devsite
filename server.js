efbad776a4dfdedbd2679bb87c8eb497f3d3b17f
Jupiter <jupiterbaudot@gmail.com>
2019-06-06 12:28:09 -0700

    Add user routes

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express()
 
 app.get('/', (req,res) => res.send("API RUNNING"))
 
+app.use('/api/users', require('./routes/api/users'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server starting up on port ${PORT}`))
