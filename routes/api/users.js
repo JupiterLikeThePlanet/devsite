164c3a94463d1f69edecd076fd49729bb370d599
Jupiter <jupiterbaudot@gmail.com>
2019-06-13 17:14:43 -0700

    Add validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
+// https://github.com/express-validator/express-validator
+// DOCUMENTATION: https://express-validator.github.io/docs/
+const {check, validationResult} = require('express-validator/check')
 
 
 // @route Get api/users
 // @desc Test Route
 // @access Public
-router.get('/', (req, res) => res.send('User Route'));
+// router.get('/', (req, res) => res.send('User Route'));
+
+// @route Get api/users
+// @desc Register User
+// @access Public
+router.post('/',[
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+], (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        console.log(req.body);
+
+        
+
+        res.send('User Route');
+    }
+);
+
+
 
 
 // @route Post api/users
--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const connectDB = require('./config/db')
 
-const connectDB = require('./config/db');
+
 
 const app = express()
 
-<<<<<<< HEAD
-
-connectDB();
-
-=======
 //connect database
 connectDB();
 
 //initialize middleware
 app.use(express.json({extended: false}))
 
->>>>>>> 1d426b2b43dea0dcca8828bebd54da53f7c00467
+
 app.get('/', (req,res) => res.send("API RUNNING"))
 
 app.use('/api/users', require('./routes/api/users'));
